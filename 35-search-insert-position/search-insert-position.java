44f146e6d57033fe18e27cb1e6a8a9d7bf25f22e
PONNAKRISHNAVAMSI <ravulaponnakrishnavamsi@gmail.com>
2026-08-25 17:05:53 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (48.64%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -1,13 +1,20 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
         
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>=target)
-            {
-                return i;
+        int ans=nums.length;
+
+        int low=0;
+        int high=nums.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]>=target){
+                ans=mid;
+                high=mid-1;
             }
-            
+            else{
+                low=mid+1;
+            }
         }
-        return nums.length;
+        return ans;
     }
 }
