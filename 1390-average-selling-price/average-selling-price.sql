cbf5fff4885c5a9f097f164a019f0490eb08a788
PONNAKRISHNAVAMSI <ravulaponnakrishnavamsi@gmail.com>
2026-09-09 14:54:41 +0530

    Time: 854 ms (68.48%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,5 +1,14 @@
 # Write your MySQL query statement below
-select p.product_id ,ifnull(round(sum(p.price*s.units)/sum(s.units),2),0) as average_price from prices p
-left join UnitsSold s
-on s.product_id=p.product_id and s.purchase_date BETWEEN p.start_date AND p.end_date
-group by p.product_id;
+
+select 
+    p.product_id,
+    ifnull(round(sum(p.price*s.units)/sum(s.units),2),0) as average_price 
+    from prices p
+left 
+    join unitssold s
+on 
+    p.product_id=s.product_id and s.purchase_date between p.start_date and p.end_date
+group by 
+    p.product_id;
+
+# Write your MySQL query statement below
