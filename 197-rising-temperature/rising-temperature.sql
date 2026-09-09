0b0d6e2d4bb9d0a5b312264c55ec81c887e5eb5b
PONNAKRISHNAVAMSI <ravulaponnakrishnavamsi@gmail.com>
2026-09-09 09:05:27 +0530

    Time: 278 ms (33.14%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,4 +1,4 @@
 -- Write your PostgreSQL query statement below
-select id from weather w
+select id  from weather w
 where temperature>(select temperature from weather 
-where recordDate=w.recordDate-1);
+                    where recordDate=w.recordDate-1)
