fa52bc7a9d344aa52e4061bad873ff221491aac9
PONNAKRISHNAVAMSI <ravulaponnakrishnavamsi@gmail.com>
2026-09-11 16:48:56 +0530

    Time: 10 ms (80.72%) | Memory: 50.6 MB (6.72%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -1,36 +1,59 @@
 class Solution {
-    public boolean ispossible(int []weights,int day,int k){
-        int temp=0;
-        for(int i=0;i<weights.length;i++){
-            int value=weights[i];
-
-            if(value>k){
-                return false;
-            }
-            if(value+temp>k){
-                day-=1;
-                temp=0;
-            }
-            temp+=value;
-            if(day==0){
-                return false;
-            }
-            
-        }
-        return true;
-    }
     public int shipWithinDays(int[] weights, int days) {
-        int l=1;
-        int r=Integer.MAX_VALUE;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(ispossible(weights,days,mid)){
-                r=mid-1;
+        int max=0;
+        int min=0;
+
+        for(int i=0;i<weights.length;i++)
+        {
+            max+=weights[i];
+
+            if(min<weights[i])
+            {
+                min=weights[i];
+            }
+        }
+
+        int low=min;
+        int high=max;
+        int ans=0;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+
+            boolean value = ispossible(weights,days,mid);
+
+            if(value)
+            {
+                ans=mid;
+                high=mid-1;
             }
             else{
-                l=mid+1;
+                low=mid+1;
             }
         }
-        return l;
+        return ans;
     }
+
+    public boolean ispossible(int []weights,int days,int capacity)
+    {
+        int load=0,d=1;
+        for(int i=0;i<weights.length;i++)
+        {
+            if(load+weights[i]>capacity)
+            {
+                d++;
+                load=weights[i];
+            }
+            else{
+                load+=weights[i];
+            }
+        }
+        if(d<=days)
+        {
+            return true;
+        }
+        return false;
+    }
+     
+    
 }
